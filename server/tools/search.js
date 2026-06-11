618679edd4b7fe6a0b0b5618ba56be2c12d3cb18
NOTyuvraj <cyuvraj980@gmail.com>
2026-06-11 23:07:03 +0530

    Allowed all cors origin

--- server/tools/search.js
+++ server/tools/search.js
@@ -4,7 +4,7 @@ export async function webSearch(query) {
   const res = await axios.post(
      "https://api.tavily.com/search",
      {query , max_results: 5},
-     {headers: {Authorization : `Bearer ${process.env.TAVILY_API_KEY}`}}
+     {headers: {"x-api-key": process.env.TAVILY_API_KEY}},
   );
   return JSON.stringify(res.data.results.map((r) =>({
     title:r.title,
