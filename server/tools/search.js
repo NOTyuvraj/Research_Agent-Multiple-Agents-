d63ae2d5a5f4196fc74edf462d909731d0dd593e
NOTyuvraj <cyuvraj980@gmail.com>
2026-06-11 23:52:42 +0530

    s

--- server/tools/search.js
+++ server/tools/search.js
@@ -1,15 +1,19 @@
 import axios from "axios";
 
 export async function webSearch(query) {
-  console.log("TAVILY PREFIX:", process.env.TAVILY_API_KEY?.slice(0, 8));
-  const res = await axios.post(
-     "https://api.tavily.com/search",
-     {query , max_results: 5},
-     {headers: {"x-api-key": process.env.TAVILY_API_KEY}},
-  );
-  return JSON.stringify(res.data.results.map((r) =>({
-    title:r.title,
-    url:r.url,
-    snippet:r.content,
-  })))
+  try {
+    const res = await axios.post(
+      "https://api.tavily.com/search",
+      { query, max_results: 5 },
+      { headers: { "x-api-key": process.env.TAVILY_API_KEY } }
+    );
+    return JSON.stringify(res.data.results.map((r) => ({
+      title: r.title,
+      url: r.url,
+      snippet: r.content,
+    })));
+  } catch (err) {
+    console.error("Tavily error:", err.response?.data);
+    throw err;
+  }
 }
